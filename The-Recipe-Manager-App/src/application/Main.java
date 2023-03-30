5350bf45e4898e18306f7c39143fee9b9f3abc3b
patelurmi <patel.urmi@northeastern.edu>
2023-03-30 12:18:49 -0400

    Changed Name

--- The-Recipe-Manager-App/src/application/Main.java
+++ The-Recipe-Manager-App/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("OnboardingPage.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setTitle("The Recipe Manager App");
 			primaryStage.setScene(scene);
