2015ce17ccb19eb3ed8494557b29beb23aa60c96
rajavishah <122663941+rajavishah@users.noreply.github.com>
2023-03-31 22:47:30 -0400

    Rajavi shah (#3)
    * Login-Test#1
    
    * Register and Login Page
    
    * Updated Register and Login Page
    
    ---------
    
    Co-authored-by: Pannaga Veeramohan <pannaga3009@gmail.com>

--- The-Recipe-Manager-App/src/application/Main.java
+++ The-Recipe-Manager-App/src/application/Main.java
@@ -3,8 +3,9 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 
 
 public class Main extends Application {
