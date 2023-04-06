9357615bffb5ef9263f2205c93af078c1027d935
pannaga3009 <pannaga3009@gmail.com>
2023-04-06 18:23:11 -0400

    resolving conflicts

--- The-Recipe-Manager-App/src/application/DetailCardController.java
+++ The-Recipe-Manager-App/src/application/DetailCardController.java
@@ -1,36 +1,100 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
-public class DetailCardController {
+public class DetailCardController implements Initializable{
+	
 	@FXML
-    private Label DetailchefName;
+	private ImageView DetailrecipeImage;
 
-    @FXML
-    private ImageView DetailrecipeImage;
+//	@FXML
+//	private Label recipeNameDetail;
 
-    @FXML
-    private Button backbtn;
+	@FXML
+	private Label chefNameDetail;
 
-    @FXML
-    private Button commentsbtn;
+	@FXML
+	private Label recipeDescriptionDetail;
 
-    @FXML
-    private Label detailName;
+	@FXML
+	private Label recipeContentsDetail;
 
-    @FXML
-    private ImageView ratingImage;
+	@FXML
+	private ImageView recipeRatingDetail;
 
-    @FXML
-    private Label recipeContents;
+	@FXML
+	private Button backbtn;
 
-    @FXML
-    private Label recipeDescription;
-    
-    
-    
-    
+	@FXML
+	private Label recipeNameDetailTo;
+	
+	@FXML
+	private Label Mymsg;
+	
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+	   
+	}
+
+	public void initData(Recipe recipe) {
+	  
+	    try {
+	    	System.out.println("----------Inside INIT DATA----------------");
+	        System.out.println("Get name"+ recipe.getName() );
+	        System.out.println("Get chef name"+ recipe.getchefName() );
+	        System.out.println("Get recipe name detail"+ recipe.getDescription() );
+	        System.out.println("Get recipe contents"+ recipe.getContents());
+	        System.out.println("**************************"+recipeNameDetailTo);
+	        
+	        System.out.println("Mymsg------" + Mymsg);
+	        Mymsg.setText("Hello");
+	        recipeNameDetailTo.setText(recipe.getName());
+	        chefNameDetail.setText(recipe.getchefName());
+	        recipeDescriptionDetail.setText(recipe.getDescription());
+	        recipeContentsDetail.setText(recipe.getContents());
+
+	        if(recipe.getRating() >= 4.0) {
+   			 recipeRatingDetail.setImage(new Image("File:assets/Four_star.png"));
+            	return;
+            }
+            else {
+            	
+           	 recipeRatingDetail.setImage(new Image("File:assets/Three_star.jpeg"));
+            	return;
+            }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    
+	}
+	
+	public void setImage(Image img) {
+		
+		 DetailrecipeImage.setImage(img);
+	}
+
+	@FXML
+	void backButtonClicked(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	Scene scene = new Scene(root);
+    	stage.setScene(scene);
+    	stage.show();
+	}
 }
